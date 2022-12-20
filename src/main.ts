781b80f09767901006d0dc72a864975d22d39754
nimit2801 <mnimitsavant@outlook.com>
2022-12-20 20:11:37 +0530

    Website working ✨

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,15 @@
 import './style.css';
-import { setupCounter } from './counter';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <a href="" target="_blank">
-      <img src="/UpsideDownMonk_Logo_Final.webp" class="logo rotate linear infinite" alt="Vite logo" />
+      <img src="./assets/UpsideDownMonk_Logo_Final.webp" class="logo rotate linear infinite" alt="upside down monk studios logo" />
     </a>
     <h3>Upside Down Monk Studios</h3>
     <div class="row">
-    <div class="column small-logos"><a href="https://www.instagram.com/udmstudios/" target="_blank"><img style="max-width:50%" src="./Intsa_Logo.png"></img></a><h5>@udmstudios<h5></div>
-    <div class="column small-logos"><a href="mailto:monkupsidedown@gmail.com" target="_blank"><img style="max-width:50%" src="./Email_Logo.png"></img></a><h5>monkupsidedown@gmail.com<h5></div>
-    <div class="column small-logos"><a href="https://wa.me/+917569647009" target="_blank"><img style="max-width:50%" src="./Whatsapp_Logo.png"></img></a><h5>75699647009<h5></div>
+    <div class="column small-logos"><a href="https://www.instagram.com/udmstudios/" target="_blank"><img style="max-width:50%" src="./assets/Intsa_Logo.png"></img></a><h5>@udmstudios<h5></div>
+    <div class="column small-logos"><a href="mailto:monkupsidedown@gmail.com" target="_blank"><img style="max-width:50%" src="./assets/Email_Logo.png"></img></a><h5>monkupsidedown@gmail.com<h5></div>
+    <div class="column small-logos"><a href="https://wa.me/+917569647009" target="_blank"><img style="max-width:50%" src="./assets/Whatsapp_Logo.png"></img></a><h5>75699647009<h5></div>
   </div>
     </div>
     `;
@@ -23,5 +22,3 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 // <div class="card">
 //   <button id="counter" type="button"></button>
 // </div>
-
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!);
